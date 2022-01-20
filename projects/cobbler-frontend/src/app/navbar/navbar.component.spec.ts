f55c4ee83479ae2db16275b93f9d416d621b756a
Chris Koerner <bowlerct@gmail.com>
2022-01-20 14:24:09 -0500

    added RouterTestingModule to navbar spec

--- projects/cobbler-frontend/src/app/navbar/navbar.component.spec.ts
+++ projects/cobbler-frontend/src/app/navbar/navbar.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
