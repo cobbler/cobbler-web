35cc7b33b99196312435613b90e1421488a24b25
Enno Gotthold <egotthold@suse.de>
2022-01-29 14:05:46 +0100

    Cobbler-Web: App - Remove redundant code

--- projects/cobbler-frontend/src/app/app.component.ts
+++ projects/cobbler-frontend/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { CobblerApiService } from 'cobbler-api';
+import {Component, Injectable, OnInit} from '@angular/core';
 
-import { Router, NavigationEnd } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import {Router} from '@angular/router';
+import {CobblerApiService} from 'cobbler-api';
 
 
 @Component({
@@ -11,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
   providers: [
     CobblerApiService,
-    { provide: 'COBBLER_URL', useValue: new URL('http://localhost/cobbler_api') }
+    {provide: 'COBBLER_URL', useValue: new URL('http://localhost/cobbler_api')}
   ]
 })
 @Injectable()
@@ -26,19 +25,8 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     const storage = window.sessionStorage.getItem('loggedIn');
     if (storage) {
-      const boolvalue = (storage === 'true');
-      this.userStatus.loggedin = boolvalue;
+      this.userStatus.loggedin = (storage === 'true');
       window.localStorage.userStatus = this.userStatus;
     }
-    /*
-     * Do we want it to scroll to top on every component change?
-     * component change/select from menu: scroll to top?
-    this.router.events.subscribe((evt) => {
-      if (evt instanceof NavigationEnd) {
-        this.router.navigated = false;
-        window.scrollTo(0, 0);
-      }
-    });
-    */
   }
 }
