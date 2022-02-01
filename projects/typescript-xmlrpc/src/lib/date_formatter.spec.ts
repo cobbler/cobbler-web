97d3c4ac3f764eae9119649f44263d8afbf1e686
Enno Gotthold <egotthold@suse.de>
2022-02-01 14:42:21 +0100

    XMLRPC-API: Create tests for all node types

--- projects/typescript-xmlrpc/src/lib/date_formatter.spec.ts
+++ projects/typescript-xmlrpc/src/lib/date_formatter.spec.ts
@@ -5,22 +5,72 @@ describe('DateFormatter', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    jasmine.clock().uninstall();
+    jasmine.clock().install();
   });
+
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  })
 
   it('DateFormatter should be created', () => {
     expect(new DateFormatter()).toBeTruthy();
   });
 
-  it('method response', () => {
-    // Arrange
-    const goodInput = new Date(0);
-    const epectedResult = '19700101T00:00:00Z';
-    const formatter = new DateFormatter(true, false, false, false, false);
+  const parameters = [
+    {colons: true, hyphens: true, local: true, ms: true, offset: true, expectedResult: "1970-01-01T01:00:00.000+01:00"},
+    {colons: true, hyphens: true, local: true, ms: true, offset: false, expectedResult: "1970-01-01T01:00:00.000"},
+    {colons: true, hyphens: true, local: true, ms: false, offset: true, expectedResult: "1970-01-01T01:00:00+01:00"},
+    {colons: true, hyphens: true, local: true, ms: false, offset: false, expectedResult: "1970-01-01T01:00:00"},
+    {colons: true, hyphens: true, local: false, ms: true, offset: true, expectedResult: "1970-01-01T00:00:00.000Z"},
+    {colons: true, hyphens: true, local: false, ms: true, offset: false, expectedResult: "1970-01-01T00:00:00.000Z"},
+    {colons: true, hyphens: true, local: false, ms: false, offset: true, expectedResult: "1970-01-01T00:00:00Z"},
+    {colons: true, hyphens: true, local: false, ms: false, offset: false, expectedResult: "1970-01-01T00:00:00Z"},
+    {colons: true, hyphens: false, local: true, ms: true, offset: true, expectedResult: "19700101T01:00:00.000+01:00"},
+    {colons: true, hyphens: false, local: true, ms: true, offset: false, expectedResult: "19700101T01:00:00.000"},
+    {colons: true, hyphens: false, local: true, ms: false, offset: true, expectedResult: "19700101T01:00:00+01:00"},
+    {colons: true, hyphens: false, local: true, ms: false, offset: false, expectedResult: "19700101T01:00:00"},
+    {colons: true, hyphens: false, local: false, ms: true, offset: true, expectedResult: "19700101T00:00:00.000Z"},
+    {colons: true, hyphens: false, local: false, ms: true, offset: false, expectedResult: "19700101T00:00:00.000Z"},
+    {colons: true, hyphens: false, local: false, ms: false, offset: true, expectedResult: "19700101T00:00:00Z"},
+    {colons: true, hyphens: false, local: false, ms: false, offset: false, expectedResult: "19700101T00:00:00Z"},
+    {colons: false, hyphens: true, local: true, ms: true, offset: true, expectedResult: "1970-01-01T010000.000+01:00"},
+    {colons: false, hyphens: true, local: true, ms: true, offset: false, expectedResult: "1970-01-01T010000.000"},
+    {colons: false, hyphens: true, local: true, ms: false, offset: true, expectedResult: "1970-01-01T010000+01:00"},
+    {colons: false, hyphens: true, local: true, ms: false, offset: false, expectedResult: "1970-01-01T010000"},
+    {colons: false, hyphens: true, local: false, ms: true, offset: true, expectedResult: "1970-01-01T000000.000Z"},
+    {colons: false, hyphens: true, local: false, ms: true, offset: false, expectedResult: "1970-01-01T000000.000Z"},
+    {colons: false, hyphens: true, local: false, ms: false, offset: true, expectedResult: "1970-01-01T000000Z"},
+    {colons: false, hyphens: true, local: false, ms: false, offset: false, expectedResult: "1970-01-01T000000Z"},
+    {colons: false, hyphens: false, local: true, ms: true, offset: true, expectedResult: "19700101T010000.000+01:00"},
+    {colons: false, hyphens: false, local: true, ms: true, offset: false, expectedResult: "19700101T010000.000"},
+    {colons: false, hyphens: false, local: true, ms: false, offset: true, expectedResult: "19700101T010000+01:00"},
+    {colons: false, hyphens: false, local: true, ms: false, offset: false, expectedResult: "19700101T010000"},
+    {colons: false, hyphens: false, local: false, ms: true, offset: true, expectedResult: "19700101T000000.000Z"},
+    {colons: false, hyphens: false, local: false, ms: true, offset: false, expectedResult: "19700101T000000.000Z"},
+    {colons: false, hyphens: false, local: false, ms: false, offset: true, expectedResult: "19700101T000000Z"},
+    {colons: false, hyphens: false, local: false, ms: false, offset: false, expectedResult: "19700101T000000Z"},
+  ]
 
-    // Act
-    const result = formatter.encodeIso8601(goodInput);
+  parameters.forEach((parameter) => {
+    // eslint-disable-next-line max-len
+    it(`encodeIso8601 with colons=${parameter.colons} hyphens=${parameter.hyphens} local=${parameter.local} ms=${parameter.ms} offset=${parameter.offset}`, () => {
+      // Arrange
+      const goodInput = new Date(Date.UTC(1970, 0, 1, 0, 0, 0));
+      jasmine.clock().mockDate(goodInput)
+      const formatter = new DateFormatter(
+        parameter.colons,
+        parameter.hyphens,
+        parameter.local,
+        parameter.ms,
+        parameter.offset
+      );
 
-    // Assert
-    expect(result).toEqual(epectedResult);
-  });
+      // Act
+      const result = formatter.encodeIso8601(goodInput);
+
+      // Assert
+      expect(result).toEqual(parameter.expectedResult);
+    });
+  })
 });
--- projects/typescript-xmlrpc/src/lib/serializer.spec.ts
+++ projects/typescript-xmlrpc/src/lib/serializer.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { serializeMethodCall } from './serializer';
+import {TestBed} from '@angular/core/testing';
+import {serializeMethodCall} from './serializer';
 
 describe('serializeMethodCall', () => {
 
@@ -18,10 +18,65 @@ describe('serializeMethodCall', () => {
     expect(serializeMethodCall('function_name', undefined, 'UTF-8')).toBe(expectedResult);
   });
 
-  it('function with parameter', () => {
+  it('function with integer parameter', () => {
     // eslint-disable-next-line max-len
     const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><int>41</int></value></param></params></methodCall>`;
     expect(serializeMethodCall('function_name', [41])).toBe(expectedResult);
+  });
+
+  it('function with double parameter', () => {
+    // eslint-disable-next-line max-len
+    const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><double>41.2</double></value></param></params></methodCall>`;
+    expect(serializeMethodCall('function_name', [41.2])).toBe(expectedResult);
+  });
+
+  it('function with boolean parameter', () => {
+    // eslint-disable-next-line max-len
+    const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><boolean>0</boolean></value></param></params></methodCall>`;
+    expect(serializeMethodCall('function_name', [false])).toBe(expectedResult);
+  });
+
+  it('function with string parameter', () => {
+    // eslint-disable-next-line max-len
+    const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><string>text</string></value></param></params></methodCall>`;
+    expect(serializeMethodCall('function_name', ['text'])).toBe(expectedResult);
+  });
+
+  it('function with date parameter', () => {
+    // eslint-disable-next-line max-len
+    const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><dateTime.iso8601>19700101T00:00:00Z</dateTime.iso8601></value></param></params></methodCall>`;
+    expect(serializeMethodCall('function_name', [new Date('1970-01-01')])).toBe(expectedResult);
+  });
+
+  it('function with array parameter', () => {
+    // eslint-disable-next-line max-len
+    const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><array><data><value><string>elem1</string></value><value><string>elem2</string></value></data></array></value></param></params></methodCall>`;
+    expect(serializeMethodCall('function_name', [
+      {'data': ['elem1', 'elem2']}
+    ])).toBe(expectedResult);
+  });
+
+  it('function with base64 parameter', () => {
+    const myBuffer = new ArrayBuffer(8)
+    const myexampledata = new Float64Array(myBuffer)
+    myexampledata[0] = 42;
+    myexampledata[0] = 42;
+    // eslint-disable-next-line max-len
+    const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><base64>Kg==</base64></value></param></params></methodCall>`;
+    expect(serializeMethodCall('function_name', [myexampledata])).toBe(expectedResult);
+  });
+
+  it('function with struct parameter', () => {
+    // eslint-disable-next-line max-len
+    const expectedResult = `<?xml version="1.0"?><methodCall><methodName>function_name</methodName><params><param><value><struct><member><name>data1</name><value><int>0</int></value></member><member><name>data2</name><value><string></string></value></member></struct></value></param></params></methodCall>`;
+    expect(serializeMethodCall('function_name',
+      [{
+        'members': [
+          {name: "data1", value: 0},
+          {name: 'data2', value: ""}
+        ]
+      }]
+    )).toBe(expectedResult);
   });
 
   it('function with multiple parameters', () => {
