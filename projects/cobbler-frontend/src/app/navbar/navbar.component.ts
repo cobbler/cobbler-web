754e69197b27362911bdf5ec4f0a86d364721dce
Enno Gotthold <egotthold@suse.de>
2022-06-27 09:53:49 +0200

    Cobbler-Frontend: Add Cobbler version to navbar

--- projects/cobbler-frontend/src/app/navbar/navbar.component.ts
+++ projects/cobbler-frontend/src/app/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {MatIconRegistry} from '@angular/material/icon';
+import {MatSnackBar} from '@angular/material/snack-bar';
 import {DomSanitizer} from '@angular/platform-browser';
 import {Router} from '@angular/router';
+import {CobblerApiService} from 'cobbler-api';
 import {Subscription} from 'rxjs';
 import {AuthGuardService} from '../services/auth-guard.service';
 import {UserService} from '../services/user.service';
@@ -14,6 +16,7 @@ import {UserService} from '../services/user.service';
 
 export class NavbarComponent {
   @Output() toggleSidenav = new EventEmitter<void>();
+  cobbler_version: String = 'Unknown';
   islogged: boolean = false;
   subscription: Subscription;
 
@@ -22,12 +25,15 @@ export class NavbarComponent {
     sanitizer: DomSanitizer,
     public authO: UserService,
     public router: Router,
-    private guard: AuthGuardService
+    private guard: AuthGuardService,
+    private _snackBar: MatSnackBar,
+    private cobblerApiService: CobblerApiService
   ) {
     iconRegistry.addSvgIcon(
       'cobbler-logo',
       sanitizer.bypassSecurityTrustResourceUrl('https://cobbler.github.io/images/logo-cobbler-new.svg')
     );
+
     this.subscription = this.authO.authorized.subscribe((value) => {
       if (value) {
         this.islogged = value;
@@ -35,6 +41,13 @@ export class NavbarComponent {
         this.islogged = false;
       }
     });
+    cobblerApiService.extended_version().subscribe((value) => {
+        this.cobbler_version = value.version
+      },
+      (error) => {
+        this.cobbler_version = 'Error'
+        this._snackBar.open(error.message, 'Close')
+      })
   }
 
   logout(): void {
