d1291eeef4d97e3f738f26fc5cedb2d175fc3d62
Enno Gotthold <egotthold@suse.de>
2024-07-01 14:37:14 +0200

    Cobbler-Frontend: Add event log viewer

--- projects/cobbler-frontend/src/app/app-events/app-events.component.ts
+++ projects/cobbler-frontend/src/app/app-events/app-events.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
+import {AuthGuardService} from "../services/auth-guard.service";
+import {COBBLER_URL, CobblerApiService} from "cobbler-api";
 
 @Component({
   selector: 'cobbler-app-events',
@@ -7,7 +11,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppEventsComponent {
 
-  constructor() {
+  constructor(
+    public authO: UserService,
+    private router: Router,
+    private guard: AuthGuardService,
+    @Inject(COBBLER_URL) url: URL,
+    private cobblerApiService: CobblerApiService,
+  ) {
+    cobblerApiService.get_events("")
+    cobblerApiService.get_event_log("")
+    // TODO: Use "get_events" to show a table of all events
+    // TODO: Have three dots at the end of row with menu entry "show logs"
+    // TODO: Have page with fixed top for event details and scrollable text field for the log
   }
 
 }
