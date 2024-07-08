3e8eb7bb502acdf3cbcf22c3c304c20511a00ed7
Enno Gotthold <matrixfueller@gmail.com>
2024-07-08 21:57:51 +0200

    Cobbler-Frontend: Add ability to view output of "cobbler check"

--- projects/cobbler-frontend/src/app/actions/check-sys/check-sys.component.spec.ts
+++ projects/cobbler-frontend/src/app/actions/check-sys/check-sys.component.spec.ts
@@ -1,6 +1,9 @@
+import {provideHttpClient} from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatListModule } from '@angular/material/list';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {provideRouter} from '@angular/router';
+import {COBBLER_URL} from 'cobbler-api';
 import { CheckSysComponent } from './check-sys.component';
 
 
@@ -10,9 +13,14 @@ describe('CheckSysComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MatListModule, CheckSysComponent],
+      imports: [MatListModule, CheckSysComponent, NoopAnimationsModule],
       providers: [
         provideRouter([]),
+        provideHttpClient(),
+        {
+          provide: COBBLER_URL,
+          useValue: new URL('http://localhost/cobbler_api')
+        },
       ]
     }).compileComponents();
   });
