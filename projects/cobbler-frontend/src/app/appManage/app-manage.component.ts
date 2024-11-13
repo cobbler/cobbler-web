273b11ce61ac5f8c2eb8fb0caef93d64966c5481
Enno Gotthold <matrixfueller@gmail.com>
2024-11-13 10:42:57 +0100

    Cobbler-Frontend: Remove unused GetObjService

--- projects/cobbler-frontend/src/app/appManage/app-manage.component.ts
+++ projects/cobbler-frontend/src/app/appManage/app-manage.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { GetObjService } from '../services/get-obj.service';
 
 @Component({
   selector: 'cobbler-app-manage',
@@ -8,38 +7,5 @@ import { GetObjService } from '../services/get-obj.service';
   standalone: true,
 })
 export class AppManageComponent {
-  username: string;
-  currentItem: string;
-  currentObjs;
-  saveObj;
-
-  constructor(service: GetObjService) {
-    const user = window.sessionStorage.getItem('user');
-    const item = window.sessionStorage.getItem('CurrentItem');
-    this.currentObjs = service.getITEMS();
-    // console.log(`Current objects: ${this.current_objs}.  Type=`, typeof this.current_objs)
-    if (user) {
-      this.username = user;
-    }
-
-    if (item) {
-      this.currentItem = item;
-    } else {
-      // console.log(`item not found: ${item}`)
-      this.currentItem = 'No Item has been selected.';
-    }
-    this.saveObj = (value) => {
-      // console.log("saving current value in service")
-      service.name = value;
-    };
-  }
-
-  // TODO: Find a way to also save this in current service
-  save(index): void {
-    // console.log('saving item')
-    // let size = this.current_objs.length
-    const current = this.currentObjs[index];
-    this.currentItem = current;
-    this.saveObj(current);
-  }
+  constructor() {}
 }
