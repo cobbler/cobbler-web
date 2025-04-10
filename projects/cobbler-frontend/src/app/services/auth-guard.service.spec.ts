5097acc8b5b33d020458ddeb6e75e8e1836f0101
Enno Gotthold <matrixfueller@gmail.com>
2025-04-10 19:02:40 +0200

    Cobbler-Frontend: Save auth token in localStorage

--- projects/cobbler-frontend/src/app/services/auth-guard.service.spec.ts
+++ projects/cobbler-frontend/src/app/services/auth-guard.service.spec.ts
@@ -1,5 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
+import { COBBLER_URL } from 'cobbler-api';
 
 import { AuthGuardService } from './auth-guard.service';
 import { UserService } from './user.service';
@@ -9,7 +10,14 @@ describe('AuthGuardService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideRouter([]), UserService],
+      providers: [
+        {
+          provide: COBBLER_URL,
+          useValue: new URL('http://localhost/cobbler_api'),
+        },
+        UserService,
+      ],
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(AuthGuardService);
   });
