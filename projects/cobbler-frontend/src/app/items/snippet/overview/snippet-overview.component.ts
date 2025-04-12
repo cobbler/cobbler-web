b9eb517b4088b8dda6cedeb4533081cbed927511
Enno Gotthold <matrixfueller@gmail.com>
2025-04-12 11:50:25 +0200

    Cobbler-Frontend: Add creation of snippet objects

--- projects/cobbler-frontend/src/app/items/snippet/overview/snippet-overview.component.ts
+++ projects/cobbler-frontend/src/app/items/snippet/overview/snippet-overview.component.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTable, MatTableModule } from '@angular/material/table';
+import { MatTooltip } from '@angular/material/tooltip';
 import { Router } from '@angular/router';
 import { CobblerApiService } from 'cobbler-api';
 import { Subject } from 'rxjs';
@@ -12,11 +13,18 @@ import { takeUntil } from 'rxjs/operators';
 import { DialogItemRenameComponent } from '../../../common/dialog-item-rename/dialog-item-rename.component';
 import { UserService } from '../../../services/user.service';
 import Utils from '../../../utils';
+import { SnippetCreateComponent } from '../create/snippet-create.component';
 
 @Component({
   selector: 'cobbler-snippet-overview',
   standalone: true,
-  imports: [MatTableModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTooltip,
+  ],
   templateUrl: './snippet-overview.component.html',
   styleUrl: './snippet-overview.component.scss',
 })
@@ -61,6 +69,17 @@ export class SnippetOverviewComponent implements OnInit, OnDestroy {
       });
   }
 
+  addSnippet(): void {
+    const dialogRef = this.dialog.open(SnippetCreateComponent, {
+      width: '40%',
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (typeof result === 'string') {
+        this.router.navigate(['/items', 'snippet', result]);
+      }
+    });
+  }
+
   showSnippet(name: string): void {
     this.router.navigate(['/items', 'snippet', name]);
   }
@@ -92,12 +111,12 @@ export class SnippetOverviewComponent implements OnInit, OnDestroy {
               )
               .pipe(takeUntil(this.ngUnsubscribe))
               .subscribe({
-                next: (value) => {
+                next: () => {
                   this.cobblerApiService
                     .remove_autoinstall_snippet(name, this.userService.token)
                     .pipe(takeUntil(this.ngUnsubscribe))
                     .subscribe({
-                      next: (value) => {
+                      next: () => {
                         this.retrieveSnippets();
                       },
                       error: (error) => {
@@ -127,7 +146,7 @@ export class SnippetOverviewComponent implements OnInit, OnDestroy {
     this.cobblerApiService
       .remove_autoinstall_snippet(name, this.userService.token)
       .subscribe({
-        next: (value) => {
+        next: () => {
           this.retrieveSnippets();
         },
         error: (error) => {
