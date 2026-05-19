f2498d67be3bcc337b9fd3676f7e7a712f73944f
cathedraal <whiteobamama@gmail.com>
2026-05-19 20:09:20 +0200

    Fix (Lint): Formatted code to fix linter errors

--- projects/cobbler-frontend/src/app/actions/check-sys/check-sys.component.spec.ts
+++ projects/cobbler-frontend/src/app/actions/check-sys/check-sys.component.spec.ts
@@ -1,5 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ComponentFixture, fakeAsync, tick, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  tick,
+  TestBed,
+} from '@angular/core/testing';
 import { MatListModule } from '@angular/material/list';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
